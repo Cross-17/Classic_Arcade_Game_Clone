a1aea0905d752dd2deaef23d5381b13f10acb005
Cross <hezhengda@163.com>
2017-02-25 14:31:43 +0800

    1.1

--- js/app.js
+++ js/app.js
@@ -80,5 +80,5 @@ document.addEventListener('keyup', function(e) {
 });
 // generate random number
 function RandomNum(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
